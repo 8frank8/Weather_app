b1c4f813a33908101cfcc1aa06cdf6d60d853858
8frank8 <77594456+8frank8@users.noreply.github.com>
2021-10-05 00:12:53 -0500

    updating js file

--- Weather App/Weather App/app.js
+++ Weather App/Weather App/app.js
@@ -1,5 +1,7 @@
 
 
+ 
+
  const weather_app = {
   
  getSearchBar: () => {
@@ -17,44 +19,52 @@
     }
    }, 
  fetchWeather: () => {
-     let lat = document.querySelector('.lat').value
-     let lon = document.querySelector('.long').value
+    //  let lat = document.querySelector('.lat').value
+    //  let lon = document.querySelector('.long').value
+    let city_value = document.querySelector('.long').value
      let key = '2f144ea17440ffa4f2e637a7a353e424'
-    let  link = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${key}`;
+    let  link = `http://api.openweathermap.org/data/2.5/weather?q=${city_value}&units=imperial&appid=${key}`;
    
 
     fetch(link)
     .then((response)=> {
-      if(!response.ok) throw new Error(response.statusText);
+      // if(!response.ok) throw new Error(response.statusText);
       return response.json()})
     .then((data) => {
       weather_app.showWeather(data);
     })
-    .catch(console.error)
+    // .catch(window.alert(error))
  
   },
    showWeather: (data) => {
     console.log(data)
     let display = document.querySelector('.row');
-    let time = data.dt;
+    let time =new Date(data.dt * 1000);
     let city = data.name;
     let icon = data.weather[0].icon;
     let current_temp = data.main.temp;
+    let feel_temp = data.main.feels_like;
+    let min_temp = data.main.temp_min;
+    let max_temp = data.main.temp_max;
+    let description = data.weather[0].description;
+    let wind_speed = data.wind.speed;
+    let humid = data.main.humidity;
 
 
     let html = `
      <div class="col">
      <h4 class="current-time">${time}</h4>
+     <br>
      <h1 id="location">${city}</h1>
      <img src="http://openweathermap.org/img/wn/${icon}.png" alt="" class="icon">
      <div class="temp-container">
+        <p class="min-temp">Minimum Temperature:${min_temp}</p>
          <p class="curren-temp">Current Temperature:${current_temp}</p>
-         <p class="min-temp">Minimum Temperature:${city}</p>
-         <p class="max-temp">Maximum Temperature:${city} </p>
+         <p class="max-temp">Maximum Temperature:${max_temp} </p>
      </div>
      <div class="description-container">
          <!-- description, feels like and windspeed -->
-         <p>${city}</p>
+         <p>Today feels like ${feel_temp} outside with ${description} and wind speeds of ${wind_speed} and a humidity of ${humid}</p>
 
        
      </div>
@@ -68,4 +78,3 @@
 };
 
   weather_app.getSearchBar();
-
