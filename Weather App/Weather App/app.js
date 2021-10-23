2e99ccadfd96dbfd7add64675f0e213ff6da6c81
8frank8 <77594456+8frank8@users.noreply.github.com>
2021-10-22 23:20:32 -0500

    Update app.js
    updated to windy api for the map

--- Weather App/Weather App/app.js
+++ Weather App/Weather App/app.js
@@ -1,8 +1,6 @@
 
 
- 
-
-const weather_app = {
+ const weather_app = {
   
  getSearchBar: () => {
     document.querySelector('.search').addEventListener('click', weather_app.fetchWeather );
@@ -36,6 +34,7 @@ const weather_app = {
     // .catch(window.alert(error))
  
   },
+ 
    showWeather: (data) => {
     console.log(data)
     let display = document.querySelector('.row');
@@ -51,57 +50,81 @@ const weather_app = {
     let humid = data.main.humidity;
     let lat = data.coord.lat;
     let lon = data.coord.lon;
+    let wind_direction = data.wind.deg;
+   let cloudiness = data.clouds.all;
+
+ 
     const myLatnLon= {lat:lat, lng:lon};
     let html = `
-     <div class="col">
-     <h4 class="current-time">${time}</h4>
-     <br>
-     <h1 id="location">${city}</h1>
-     <img src="http://openweathermap.org/img/wn/${icon}@4x.png" alt="" class="icon">
-     <div class="temp-container">
-        <p class="min-temp">Minimum Temperature:${min_temp}</p>
-         <p class="curren-temp">Current Temperature:${current_temp}</p>
-         <p class="max-temp">Maximum Temperature:${max_temp} </p>
-     </div>
-     <div class="description-container">
-         <!-- description, feels like and windspeed -->
-         <p>Today feels like ${feel_temp} outside with ${description} and wind speeds of ${wind_speed} and a humidity of ${humid}</p>
+    <div class="col">
+    <div class = "title">
+        <div class ="location">
+    <p class="current-time">${time}</p>
+    <p id="location">${city}</p>
+</div>
 
-       
-     </div>
- </div>
+    <div class = "img_container">
+    <img src="http://openweathermap.org/img/wn/${icon}@4x.png" alt="icon" class="icon">
+    <div class = "temp_cont">
+    <p class = "temp">${Math.round(current_temp)}</p>
+    </div>
+    </div>
+    </div>
+    <div class="description_container">
+        <!-- description, feels like and windspeed -->
+        <p> Today feels like ${Math.round(feel_temp)} outside with ${description} </p>
+    </div>
+   
+  <ul>
+      <li class="li_item">Minimum Temperature:${Math.round(min_temp)}</li>
+      <li class="li_item"">Maximum Temperature:${Math.round(max_temp)}</li>
+      <li class="li_item"">${wind_direction} and  ${Math.round(wind_speed)}  </li>
+      <li class="li_item"">Humidity: ${Math.round(humid)}</li>
+      <li class="li_item"">Cloudiness: ${Math.round(cloudiness)}</li>  
+    </ul>
+    <div id="windy"></div>
+
+</div>
+
      `
      display.innerHTML = html;
-  
-     map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: lat, lng:lon },
-      zoom: 11,
-    }
-    )
-    // let image = `http://openweathermap.org/img/wn/${icon}@4x.png`
-    new google.maps.Marker({
-      position: myLatnLon,
-      map,
-      // icon:image,
-    });
-   }
-  
-  // initmap:(data) => {
-  //   let lat = data.coord.lat;
-  //   let lon = data.coord.lon;
-  //   const myLatnLon= {lat:lat, lng:lon};
+    
+   
 
-  //   map = new google.maps.Map(document.getElementById("map"), {
-  //     center: { lat: lat, lng:lon },
-  //     zoom: 10,
-  //   }
-  //   )
-  //   new google.maps.Marker({
-  //     position: myLatnLon,
-  //     map,
-  //     title: "Hello World!",
-  //   });
-  // }
+
+     const options = {
+      // Required: API key
+      key: 'B0hBFdgPfnNvtAmcaD8xClzoZIwgTEsQ', // REPLACE WITH YOUR KEY !!!
+  
+      // Put additional console output
+  
+      // Optional: Initial state of the map
+      lat: lat,
+      lon: lon,
+      numDirection: true,
+     }
+  
+  // Initialize Windy API
+  windyInit(options, windyAPI => {
+    
+  
+  
+      const { map } = windyAPI;
+      // .map is instance of Leaflet map
+    
+      L.popup()
+          .setLatLng([lat, lon])
+          .setContent(`${current_temp} ${city}`)
+          .openOn(map)
+        
+  });
+    
+  
+   },
+  
+ 
+  
+ 
 };
 
 
@@ -111,3 +134,4 @@ const weather_app = {
 
 
   weather_app.getSearchBar();
+
